5da47ed86d3f9ea3ab964ce362f1e9b9652e6bfc
Samuel Sierra <ssierra@wpi.edu>
2021-01-14 16:56:22 -0500

    changes for heroku

--- dna/models.py
+++ dna/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class DNA(models.Model):
     sequence=models.CharField(max_length=100)
-    # matching_partner=models.CharField(max_length=300)
+    matching_partner=models.CharField(max_length=300)
     checked_at=models.DateField(auto_now_add=True)
